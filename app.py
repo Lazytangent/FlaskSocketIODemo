1cffc6422a44a19750717fd2db43d010277bd699
Lazytangent <pm123633@gmail.com>
2021-03-01 20:38:12 -0600

    Update stuff to include socketio

--- app.py
+++ app.py
@@ -1,13 +1,43 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from threading import Lock
+from flask import Flask, render_template, session, copy_current_request_context
+from flask_socketio import SocketIO, emit, disconnect
+
 async_mode = None
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
 socket_ = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
 
 
 @app.route('/')
 def index():
     return render_template('index.html', sync_mode=socket_.async_mode)
+
+
+@socket_.on('my_event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response', {'data': message['data'], 'count':
+                         session['receive_count']})
+
+
+@socket_.on('my_broadcast_event', namespace='/test')
+def test_broadcast_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response', {'data': message['data'], 'count':
+                         session['receive_count']}, broadcast=True)
+
+
+@socket_.on('disconnect_request', namespace='/test')
+def disconnect_request():
+    @copy_current_request_context
+    def can_disconnect():
+        disconnect()
+
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response', {'data': 'Disconnected!', 'count':
+                         session['receive_count']}, callback=can_disconnect)
 
 
 if __name__ == '__main__':
